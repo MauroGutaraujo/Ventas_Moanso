9af7745d6fe1a664ee89d586b7d4ecaeddf9c32e
JairQuiroz01 <junior.agosto.rc@gmail.com>
2025-05-13 02:29:18 -0500

    esta es la real

---
+++ Ventas_Moanso/Conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace VentaDirecta
+{
+    internal class Conexion
+    {
+    }
+}
+
+public class Conexion
+{
+    private SqlConnection conn = new SqlConnection("Server=localhost;Database=Basededatos_VinoAR;Trusted_Connection=True;");
+
+    public SqlConnection AbrirConexion()
+    {
+        if (conn.State == System.Data.ConnectionState.Closed)
+            conn.Open();
+        return conn;
+    }
+
+    public void CerrarConexion()
+    {
+        if (conn.State == System.Data.ConnectionState.Open)
+            conn.Close();
+    }
+}
